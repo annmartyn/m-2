04012b8d56cd9cbdf01a2324c5f9159db89815c8
annmartyn <yutamar@yandex.ru>
2021-11-25 14:52:33 +0300

    add structure

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+};
